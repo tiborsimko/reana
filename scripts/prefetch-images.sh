9eed431d1e6fbe677de91b75b2e7fecd9ec5a1e9
Tibor Šimko <tibor.simko@cern.ch>
2025-04-25 18:22:20 +0200

    feat(reana-dev): allow using Colima/K3s as an alternative to Kind (#894)
    Add option `--kubernetes` to several `reana-dev` commands to allow using
    Colima/K3s as an alternative to Kind. Useful on macOS where a VM is
    necessary to run Docker anyway. Developing locally in Colima/K3s in the
    `latest` cluster mode saves time, because the built container images are
    natively present in the VM, and it is not necessary anymore to load and
    reload Docker images to the Kind cluster.

--- scripts/prefetch-images.sh
+++ scripts/prefetch-images.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 #
 # This file is part of REANA.
-# Copyright (C) 2020, 2021, 2022, 2023, 2024 CERN.
+# Copyright (C) 2020, 2021, 2022, 2023, 2024, 2025 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
+# Are we using Kind for local developments? (kind, colima/k3s)
+if [ -z "$1" ]; then
+  kubernetes=kind
+else
+  kubernetes="$1"
+fi
+
 for image in \
-    docker.io/maildev/maildev:1.1.0 \
-    docker.io/library/postgres:12.13 \
-    docker.io/library/redis:5.0.5 \
-    docker.io/reanahub/reana-job-controller:0.95.0-alpha.2 \
-    docker.io/reanahub/reana-message-broker:0.95.0-alpha.1 \
-    docker.io/reanahub/reana-server:0.95.0-alpha.3 \
-    docker.io/reanahub/reana-ui:0.95.0-alpha.1 \
-    docker.io/reanahub/reana-workflow-controller:0.95.0-alpha.4 \
-    docker.io/reanahub/reana-workflow-engine-cwl:0.95.0-alpha.2 \
-    docker.io/reanahub/reana-workflow-engine-serial:0.95.0-alpha.2 \
-    docker.io/reanahub/reana-workflow-engine-snakemake:0.95.0-alpha.2 \
-    docker.io/reanahub/reana-workflow-engine-yadage:0.95.0-alpha.2 \
-    quay.io/jupyter/scipy-notebook:notebook-7.2.2; do
-    docker pull $image
+  docker.io/maildev/maildev:1.1.0 \
+  docker.io/library/postgres:12.13 \
+  docker.io/library/redis:5.0.5 \
+  docker.io/reanahub/reana-job-controller:0.95.0-alpha.2 \
+  docker.io/reanahub/reana-message-broker:0.95.0-alpha.1 \
+  docker.io/reanahub/reana-server:0.95.0-alpha.3 \
+  docker.io/reanahub/reana-ui:0.95.0-alpha.1 \
+  docker.io/reanahub/reana-workflow-controller:0.95.0-alpha.4 \
+  docker.io/reanahub/reana-workflow-engine-cwl:0.95.0-alpha.2 \
+  docker.io/reanahub/reana-workflow-engine-serial:0.95.0-alpha.2 \
+  docker.io/reanahub/reana-workflow-engine-snakemake:0.95.0-alpha.2 \
+  docker.io/reanahub/reana-workflow-engine-yadage:0.95.0-alpha.2 \
+  quay.io/jupyter/scipy-notebook:notebook-7.2.2; do
+  docker pull $image
+  if [ "$kubernetes" == "kind" ]; then
     kind load docker-image $image
+  fi
 done
